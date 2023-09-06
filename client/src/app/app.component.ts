09343fb48eff4a2b3e1b5a641ddff724c6beb734
FIRST_NAME LAST_NAME <sirKhanh195@gmail.com>
2023-09-06 10:15:57 +0700

    section 5- login and register

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
@@ -8,16 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'Dating App!';
-  users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {
-    this.http.get('http://localhost:5069/api/users/').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log("Request completed")
-      
-    })
+    this.setCurrentUser()
+  }
+  
+  setCurrentUser() {
+    const userSring = localStorage.getItem('user');  
+    if (!userSring) return;
+    const user: User = JSON.parse(userSring);
+    this.accountService.setCurrentUser(user);
   }
 }
