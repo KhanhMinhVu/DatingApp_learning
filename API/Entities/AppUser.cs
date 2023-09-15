4f3a8dcd988351d9b2ae053d070d37c58c995ed7
FIRST_NAME LAST_NAME <sirKhanh195@gmail.com>
2023-09-15 11:54:22 +0700

    End of Section 8

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+using API.Extensions;
 
 namespace API.Entities
 {
@@ -25,5 +21,9 @@ namespace API.Entities
         public string City { get; set; }
         public string Country { get; set; }
         public List<Photo> Photos { get; set; } = new List<Photo>();
+
+        // public int GetAge() {
+        //     return DateOfBirth.CalculateAge();
+        // }
     }
 }
--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -1,6 +1,5 @@
 using System.Net;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using API.Errors;
 
 namespace API.Middleware
